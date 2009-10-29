589a68f61004ff06eb21b554b155b4a55fe0574f
Will Benton <willb@redhat.com>
2009-10-29 16:07:54 -0500

    Fixes to Hello example

--- manageable-example.rb
+++ manageable-example.rb
@@ -1,20 +1,33 @@
-require 'manageable'
+require 'spqr/spqr'
+require 'spqr/app'
+require 'logger'
 
 class Hello
    include SPQR::Manageable
    def hello(args)
-     args[:result] = "Hello, #{args[:name]}!"
+     args["result"] = "Hello, #{args['name']}!"
    end
 
    spqr_expose :hello do |args|
-     args.declare :name, :string, :in
-     args.declare :result, :string, :out
+     args.declare :name, :lstr, :in
+     args.declare :result, :lstr, :out
    end
 
    spqr_package :hello
    spqr_class :Hello
    spqr_statistic :people_greeted, :int
-   spqr_property :service_name, :string
+   spqr_property :service_name, :lstr
+   
+   def Hello.find_all 
+     [Hello.new]
+   end
+
+   def Hello.find_by_id(id)
+     Hello.new
+   end
 end
 
-p Hello.spqr_meta
+app = SPQR::App.new(:loglevel => :debug)
+app.register Hello
+
+app.main
