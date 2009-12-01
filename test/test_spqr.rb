bc0b06665500497c2be0b19e6f0cdcd136142ef9
Will Benton <willb@redhat.com>
2009-11-30 23:39:35 -0600

    two basic tests

--- test/test_spqr.rb
+++ test/test_spqr.rb
@@ -1,7 +1,81 @@
 require 'helper'
+require 'qmf'
+
+class QmfHello
+  include ::SPQR::Manageable
+
+  def hello(args)
+    args["result"] = "Hello, #{args['name']}!"
+  end
+
+  def self.find_by_id(oid)
+    @@singleton ||= QmfHello.new
+    @@singleton
+  end
+
+  def self.find_all
+    @@singleton ||= QmfHello.new
+    [@@singleton]
+  end
+
+  spqr_expose :hello do |args|
+    args.declare :name, :lstr, :in
+    args.declare :result, :lstr, :out
+  end
+  
+  spqr_package :example
+  spqr_class :QmfHello
+end
+
+module QmfTestHelpers
+  def app_setup(*classes)
+    @app = SPQR::App.new(:loglevel => :fatal)
+    @app.register *classes
+    @child_pid = fork do 
+      # replace stdin/stdout/stderr
+      $stdin.reopen("/dev/null", "r")
+      $stdout.reopen("/dev/null", "w")
+      $stderr.reopen("/dev/null", "w")
+
+      @app.main
+    end
+    
+    sleep 1
+    @connection = Qmf::Connection.new(Qmf::ConnectionSettings.new)
+    @console = Qmf::Console.new
+    @broker = @console.add_connection(@connection)
+    @broker.wait_for_stable
+  end
+
+  def teardown
+    @connection = nil
+    Process.kill(1, @child_pid) if @child_pid
+  end
+end
 
 class TestSpqr < Test::Unit::TestCase
-  def test_something_for_real
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  include QmfTestHelpers
+
+  def setup
+    @child_pid = nil
+  end
+  
+  def test_hello_objects
+    app_setup QmfHello
+    objs = @console.objects(:class=>"QmfHello")
+    assert objs.size > 0
+  end
+
+  def test_hello_call
+    app_setup QmfHello
+    obj = @console.objects(:class=>"QmfHello")[0]
+    
+    val = obj.hello("ruby").result
+    args = { 'name' => 'ruby' }
+    QmfHello.find_by_id(0).hello(args)
+
+    expected = args['result']
+
+    assert_equal expected, val
   end
 end
