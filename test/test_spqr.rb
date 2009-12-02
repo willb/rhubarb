514e49f47071687dceea7074c69da03d42d5050b
Will Benton <willb@redhat.com>
2009-12-02 15:26:58 -0600

    Added some SPQR tests.

--- test/test_spqr.rb
+++ test/test_spqr.rb
@@ -1,17 +1,75 @@
 require 'helper'
 require 'qmf'
+require 'set'
+require 'timeout'
+
+class QmfDummyProp
+  include ::SPQR::Manageable
+
+  def QmfDummyProp.find_by_id(oid)
+    @qmf_dps ||= [QmfDummyProp.new]
+    @qmf_dps[0]
+  end
+  
+  def QmfDummyProp.find_all
+    @qmf_dps ||= [QmfDummyProp.new]
+    @qmf_dps
+  end
+  
+  def service_name
+    "DummyPropService"
+  end
+  
+  spqr_property :service_name, :lstr
+
+  spqr_class :QmfDummyProp
+  spqr_package :example
+end
+
+class QmfIntegerProp
+  include ::SPQR::Manageable 
+
+  SIZE = 12
+ 
+  def initialize(oid)
+    @int_id = oid
+  end
+  
+  def QmfIntegerProp.gen_objects(ct)
+    objs = []
+    ct.times do |x|
+      objs << (new(x))
+    end
+    objs
+  end
+
+  def QmfIntegerProp.find_by_id(oid)
+    @qmf_ips ||= gen_objects SIZE
+    @qmf_ips[oid]
+  end
+  
+  def QmfIntegerProp.find_all
+    @qmf_ips ||= gen_objects SIZE
+    @qmf_ips
+  end
+  
+  spqr_property :int_id, :int
+
+  spqr_class :QmfIntegerProp
+  spqr_package :example
+end
 
 class QmfHello
   include ::SPQR::Manageable
-
+  
   def QmfHello.find_by_id(oid)
-    @@qmf_hellos ||= [QmfHello.new]
-    @@qmf_hellos[0]
+    @qmf_hellos ||= [QmfHello.new]
+    @qmf_hellos[0]
   end
   
   def QmfHello.find_all
-    @@qmf_hellos ||= [QmfHello.new]
-    @@qmf_hellos
+    @qmf_hellos ||= [QmfHello.new]
+    @qmf_hellos
   end
 
   def hello(args)
@@ -28,37 +86,43 @@ class QmfHello
 end
 
 class QmfClicker
-   include ::SPQR::Manageable
-
-   def QmfClicker.find_by_id(oid)
-     @singleton ||= QmfClicker.new
-     @singleton
-   end
+  include ::SPQR::Manageable
   
-   def QmfClicker.find_all
-     @singleton ||= QmfClicker.new
-     [@singleton]
-   end
-
-   def initialize
-     @clicks = 0
-   end
-
-   def click(args)
-     @clicks = @clicks.succ
-   end
+  def QmfClicker.find_by_id(oid)
+    @singleton ||= QmfClicker.new
+    @singleton
+  end
   
-   spqr_expose :click do |args| 
-   end
-
-   spqr_statistic :clicks, :int
-
-   spqr_package :example
-   spqr_class :QmfClicker
- end
+  def QmfClicker.find_all
+    @singleton ||= QmfClicker.new
+    [@singleton]
+  end
+  
+  def initialize
+    @clicks = 0
+  end
+  
+  def click(args)
+    @clicks = @clicks.succ
+  end
+  
+  spqr_expose :click do |args| 
+  end
+  
+  spqr_statistic :clicks, :int
+  
+  spqr_package :example
+  spqr_class :QmfClicker
+end
 
 module QmfTestHelpers
   def app_setup(*classes)
+    $connection = Qmf::Connection.new(Qmf::ConnectionSettings.new) unless $connection
+    $console = Qmf::Console.new unless $console
+    $broker = $console.add_connection($connection) unless $broker
+
+    $broker.wait_for_stable
+
     pr, pw = IO.pipe
     @app = SPQR::App.new(:loglevel => :fatal, :notifier => pw)
     @app.register *classes
@@ -73,16 +137,13 @@ module QmfTestHelpers
       @app.main
     end
 
-    sleep 0.25
+    sleep 0.35
 
-    pw.close
-    pr.read
+    Timeout.timeout(15) do
+      pw.close
+      pr.read
+    end
 
-    $connection = Qmf::Connection.new(Qmf::ConnectionSettings.new) unless $connection
-    $console = Qmf::Console.new unless $console
-    $broker = $console.add_connection($connection) unless $broker
-
-    $broker.wait_for_stable
   end
 
   def teardown
@@ -134,4 +195,56 @@ class TestSpqr < Test::Unit::TestCase
     assert_equal 0, obj[:clicks]
   end
 
+  def test_statistics_postquery
+    app_setup QmfClicker
+
+    x = 0
+    
+    9.times do
+      obj = $console.objects(:class=>"QmfClicker")[0]
+      assert_equal x, obj[:clicks]
+      
+      obj.click({})
+      x = x.succ
+    end
+  end
+
+  def test_statistics_postupdate
+    app_setup QmfClicker
+
+    x = 0
+    obj = $console.objects(:class=>"QmfClicker")[0]
+    
+    9.times do
+      obj.update
+      assert_equal x, obj[:clicks]
+      
+      obj.click({})
+      x = x.succ
+    end
+  end
+
+  def test_property_basic
+    app_setup QmfDummyProp
+
+    obj = $console.objects(:class=>"QmfDummyProp")[0]
+    assert_equal "DummyPropService", obj[:service_name]
+  end
+
+  def test_property_identities
+    app_setup QmfIntegerProp
+
+    objs = $console.objects(:class=>"QmfIntegerProp")
+    ids = Set.new
+
+    objs.each do |obj| 
+      ids << obj[:int_id] 
+    end
+
+    assert_equal objs.size, ids.size
+    
+    objs.size.times do |x|
+      assert ids.include?(x), "ids should include #{x}, which is less than #{objs.size}"
+    end
+  end
 end
